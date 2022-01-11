5c4808969afdfe38f5101fe362cbd7e0f7acad64
Ewan Selkirk <ewanselkirk@sky.com>
2022-01-11 03:03:42 +0000

    First iteration of customizable metadata The metadata visible can be modified using the button in the top bar. The UI is very rudimentary (I'm not good at front-end). Currently cannot configure bools (still to be implemented). Custom themes should be supported, but will need to be checked. Changed many hard-coded element references to 'lastChild' references. Updated Readme.md w/ list of tokens, known bugs, a screenshot & credits

--- Readme.md
+++ Readme.md
@@ -17,5 +17,24 @@ It's always annoyed the hell out of me that Spotify doesn't show when a track or
  4. Run `spicetify config extensions metadataCustomizer.js` to enable the extension.
  5. Run `spicetify apply` to apply the changes and restart Spotify.
 
+## List of Tokens
+| Token          | Value                                       |
+|----------------|---------------------------------------------|
+| $release_date$ | The date and year the track released        |
+| $tracks$       | Number of tracks in the album               |
+| $discs$        | Number of discs in the album                |
+| $disc_ratio$   | The ratio of # of tracks to disc (E.G. 7/8) |
+| $length$       | The length of the album*                    |
+
+Got a suggestion? Feel free to [make an issue](https://github.com/Ewan-Selkirk/Metadata-Customizer/issues/new?assignees=Ewan-Selkirk&labels=enhancement&template=token-request.md&title=%5BREQUEST%5D+) and I'll look into whether it's possible!
+
+## Known Issues
+- The Spotify album API has a hard limit of 50 tracks, meaning any album with more than 50 tracks will only show as having 50 tracks. This is in the process of being fixed.
+- Longer albums won't have an accurate length and will instead show "about x hr".
+
 ## Screenshots
-**I'll include some screenshots when the project is more complete**
+![alt text](assets/preview.jpg "Screenshot of the Spotify window with customized metadata")
+
+## Acknowledgements
+- [bookmark.js](https://github.com/khanhas/spicetify-cli/blob/master/Extensions/bookmark.js) for demonstrating how to make a pretty configuration menu and giving me a lot of pointers
+- [Spicetify-Comfy](https://github.com/NYRI4/Comfy-spicetify) theme which is used in the screenshots
--- metadataCustomizer.js
+++ metadataCustomizer.js
@@ -2,22 +2,41 @@
 
 // NAME: MetadataCustomizer
 // AUTHOR: Ewan Selkirk
-// VERSION: 0.2.3
+// VERSION: 0.3
 // DESCRIPTION: A Spicetify extension that allows you to customize how much track/album metadata is visible
 
 /// <reference path="../../globals.d.ts" />
 
 (function MetadataCustomizer() {
-	const {Player, Platform, CosmosAsync, SVGIcons} = Spicetify;
+	const {Player, Platform, CosmosAsync, LocalStorage, React, SVGIcons} = Spicetify;
 	const data_types = ["release_date", "tracks", "discs", "disc_ratio", "length"]
-	const default_filters = ["$release_date$", "$tracks$, $discs$ [$disc_ratio$]", "$length$"]
-	const default_icons = ["enhance", "album", "clock"]
+
+	// Local Storage Token
+	const STORAGE_TOKEN = "Metadata_Customizer";
+	// Object for storing our loaded config
+	let config = {}
 
 	// Make sure Spicetify has actually initiated before trying to run the extension
-	if(!(Player && Platform && CosmosAsync && SVGIcons)){
-		setTimeout(MetadataCustomizer, 500);
+	if(!(Player && Platform && CosmosAsync && LocalStorage && React && SVGIcons)){
+		setTimeout(MetadataCustomizer, 750);
 		return
 	}
+
+	// Create new top bar button for customizing metadata
+	new Spicetify.Topbar.Button(
+		"Customize Metadata",
+		`<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor"><path d="M5 2a1 1 0 000 2h9a1 1 0 100-2H5zM8 7a1 1 0 000 2h6a1 1 0 100-2H8zM4 13a1 1 0 011-1h9a1 1 0 110 2H5a1 1 0 01-1-1zM2.707 5.293a1 1 0 00-1.414 1.414L2.586 8 1.293 9.293a1 1 0 001.414 1.414l2-2a1 1 0 000-1.414l-2-2z"/></svg>`,
+		(self) => {
+			const bound = self.element.getBoundingClientRect();
+			// Create a new instance of the configuration menu
+			const config_menu = new ConfigurationMenu();
+			config_menu.setPosition(bound.left, bound.top);
+			document.body.append(config_menu.background)
+		}
+	);
+
+	// Create/Load configuration file
+	CheckStorage();
 
 	// If Spotify starts on a Album/Track page, do the thing
 	CheckPage();
@@ -30,7 +49,7 @@
 
 	async function ModifyMetadata() {
 		let header = document.querySelector(".main-entityHeader-headerText");
-		let metadata = header.children[2];
+		let metadata = header.lastChild;
 
 		let metadata_promise = new Promise(async function(resolve) {
 			let details = await CosmosAsync.get("https://api.spotify.com/v1/albums/" + Platform.History.location.pathname.split("/")[2])
@@ -46,8 +65,8 @@
 				tracks: (details.tracks.total.toString() + (details.tracks.total === 1 ? " track" : " tracks")),
 				// Return # of disc(s) & the ratio of track to disc
 				// or just "1 disc" if only one disc
-				discs: Object.keys(disc_count).length.toString() + (Object.keys(disc_count).length === 1 ? " disc" : " discs"),
-				disc_ratio: Object.values(disc_count).toString().replaceAll(",", "/"),
+				discs: config["bools"]["showDiscCountIfSingle"] ? Object.keys(disc_count).length.toString() + (Object.keys(disc_count).length === 1 ? " disc" : " discs") : "",
+				disc_ratio: config["bools"]["showDiscCountIfSingle"] ? Object.values(disc_count).toString().replace(/,/g, "/") : "",
 				// @ts-expect-error
 				length: metadata.lastChild.innerText.split(", ")[1] ?? "Unavailable"
 			})
@@ -58,10 +77,10 @@
 
 		for (var i = 0; i < 3; i++){
 			let nestedHeader = MakeNewHeader(header);
-			let customization = new Customization(data_types, default_filters[i], new_metadata);
+			let customization = new Customization(data_types, config["filters"][i], new_metadata);
 
 			// Create elements for the details
-			let icon = CreateSVG(SVGIcons[default_icons[i]]);
+			let icon = CreateSVG(SVGIcons[config["icons"][i]]);
 			nestedHeader.appendChild(icon);
 			let newElement = nestedHeader.appendChild(document.createElement("span"));
 
@@ -74,6 +93,33 @@
 		for (var i = 0; i < 2; i++){
 			metadata.lastChild.remove();
 		}
+	}
+
+	// Check if the extension has a previously saved config
+	// If not, create a default config
+	function CheckStorage(){
+		const default_filters = ["$release_date$", "$tracks$, $discs$ [$disc_ratio$]", "$length$"]
+		const default_icons = ["enhance", "album", "clock"]
+		const object = {"filters": default_filters, "icons": default_icons, "bools": {"allowScInToken": false, "showDiscCountIfSingle": false}}
+
+		if(LocalStorage.get(STORAGE_TOKEN) !== null) {
+			config = JSON.parse(LocalStorage.get(STORAGE_TOKEN));
+		} else {
+			LocalStorage.set(STORAGE_TOKEN, JSON.stringify(object))
+			config = object;
+		}
+	}
+
+	function SaveToStorage(){
+		for (let i = 0; i < 3; i++){
+			// @ts-expect-error
+			config["filters"][i] = document.getElementById(`metadata-config-filter-${i === 0 ? "one" : i === 1 ? "two" : "three"}`).value;
+			// @ts-expect-error
+			config["icons"][i] = document.getElementById(`metadata-config-icon-${i === 0 ? "one" : i === 1 ? "two" : "three"}`).value;
+		}
+
+		LocalStorage.set(STORAGE_TOKEN, JSON.stringify(config));
+		console.log("Config Saved")
 	}
 
 	// Check if the current page is an album
@@ -109,6 +155,145 @@
 		return nestedHeader;
 	}
 
+	// Customization Configuration Menu \\
+	function CreateConfigMenu(){
+		const background = document.createElement("div");
+		background.id = "metadata-customization-config"
+		background.className = "";
+		background.style.zIndex = "1029";
+
+		const style = document.createElement("style")
+		style.textContent = `
+#metadata-customization-config {
+	position: absolute;
+	left: 0;
+	right: 0;
+	width: 100vw;
+	height: 100vw;
+	z-index: 5000;
+}
+
+#metadata-customization-options {
+	display: inline-block;
+	width: 55%;
+	min-width: 465px;
+	max-height: 15%;
+	overflow: hidden auto;
+	padding-bottom: 10px;
+	position: absolute;
+	z-index: 5001;
+}
+
+.metadata-config-icon-input {
+	margin-left: 8px;
+	border-radius: 4px;
+	padding: 0 8px 0 12px;
+	height: 32px;
+	align-items: center;
+	background: var(--spice-card);
+	border: 0;
+	color: var(--spice-text);
+}
+
+.metadata-config-filter-label {
+	width: 150px;
+}
+
+.metadata-config-filter-input {
+	color: var(--spice-text);
+	background: var(--spice-card);
+	margin-left: 8px;
+	padding: 0 8px 0 12px;
+}
+
+.metadata-config-btn-apply {
+	background: var(--spice-button);
+	color: var(--spice-text);
+	padding: 5px;
+}
+`;
+		const children = document.createElement("div");
+		children.id = "metadata-customization-options";
+		children.className = "main-contextMenu-menu";
+		children.onclick = (event) => event.stopPropagation();
+
+		background.append(style, children);
+		return {background, children};
+	}
+
+	function CreateConfigOptions(parent){
+		let font_color = "var(--spice-text)";
+
+		let title = document.createElement("h2")
+		title.innerText = "Metadata Customization";
+		title.style.color = font_color;
+
+		let valid_tokens = document.createElement("h5");
+		valid_tokens.innerText = `Valid Tokens:	${"$" + data_types.toString().replace(/,/g, "$, $") + "$"}`;
+		valid_tokens.style.color = font_color;
+
+
+		parent.append(title, valid_tokens);
+
+		// Metadata Lines
+		for (var i = 0; i < 3; i++) {
+			let label = i === 0 ? "one" : i === 1 ? "two" : "three";
+
+			let metadata_icon_line = document.createElement("div");
+			let metadata_filter_line = document.createElement("div");
+
+			let icon_label = document.createElement("label");
+			let icon_input = document.createElement("select");
+
+			icon_label.setAttribute("for", "metadata-config-icon-" + label);
+			icon_label.innerText = "Icon: ";
+			icon_label.style.color = font_color;
+
+			icon_input.className = "GlueDropdown metadata-config-icon-input"
+			icon_input.onclick = (event) => event.stopPropagation();
+			icon_input.id = "metadata-config-icon-" + label;
+
+			// Add every SVG icon as an option to the select element
+			Object.keys(SVGIcons).sort().forEach(element => {
+				let option = document.createElement("option")
+				option.text = element;
+				icon_input.append(option);
+			});
+
+			// Set the selected option to the index of the option loaded from local storage
+			icon_input.options[Object.keys(SVGIcons).sort().findIndex(
+				icon => icon === config["icons"][i])].selected = true;
+
+			let filter_label = document.createElement("label")
+			let filter_input = document.createElement("input")
+
+			filter_label.setAttribute("for", "metadata-config-filter-" + label)
+			filter_label.innerText = `Metadata Line ${label[0].toUpperCase() + label.substring(1)}: `
+			filter_label.className = "metadata-config-filter-label";
+			filter_label.style.color = font_color;
+
+			filter_input.type = "text";
+			filter_input.name = "metadata-config-filter-" + label;
+			filter_input.id = "metadata-config-filter-" + label;
+			filter_input.className = "metadata-config-filter-input";
+			filter_input.value = config["filters"][i];
+
+			metadata_icon_line.append(icon_label, icon_input)
+			metadata_filter_line.append(filter_label, filter_input);
+			parent.append(metadata_icon_line, metadata_filter_line);
+		}
+
+		let apply_button = document.createElement("button");
+		apply_button.innerText = "Apply Config";
+		apply_button.className = "metadata-config-btn-apply"
+		apply_button.onclick = (event) => {
+			SaveToStorage();
+			event.stopPropagation();
+		};
+
+		parent.append(apply_button);
+	}
+
 	class Customization{
 		/**
 		 * Constructor for the Customization Class
@@ -135,8 +320,9 @@
 
 			// Replace all instances of the token with the data
 			for (var t = 0; t < found.length; t++){
-				// @ts-expect-error
-				new_input = new_input.replaceAll(`\$${found[t]}\$`, this.data[found[t]])
+				new_input = new_input.replace(new RegExp("(?<Opening>(?:, *)*[\\(\\[\\{]*)*(?<Token>\\$" + 
+					`(${found[t]})` + "\\$)(?<Closing>[\\}\\]\\)]*)*", "g"), 
+				"$<Opening>" + this.data[found[t]] + "$<Closing>")
 			}
 
 			// Return the new string
@@ -157,6 +343,26 @@
 
 			// Return the array
 			return found_tokens;
+		}
+	}
+
+	class ConfigurationMenu {
+		constructor(){
+			const menu = CreateConfigMenu();
+
+			this.background = menu.background;
+			this.children = menu.children;
+
+			this.background.onclick = () => {
+				this.background.remove();
+			}
+
+			CreateConfigOptions(this.children);
+		}
+
+		setPosition(x, y){
+			this.children.style.left = x + "px";
+			this.children.style.top = y + 40 + "px";
 		}
 	}
 })();
